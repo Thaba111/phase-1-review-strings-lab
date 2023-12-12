4e7513b12b176fa5689596dc4e4a6f1856e0bdb0
Edith Thaba <writersherry5@gmail.com>
2023-12-12 06:39:15 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+expect(excitedWelcomeMessage).to.equal('WELCOME TO FLATBOOK, GRACE HOPPER!'); 
